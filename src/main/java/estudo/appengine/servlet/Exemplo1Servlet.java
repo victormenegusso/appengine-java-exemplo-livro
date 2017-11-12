597c50d6015f29120802c754de0ecbf9a8a7dbed
Victor Menegusso <victormenegusso@gmail.com>
2017-11-12 00:58:26 -0200

    primeiro commit

---
+++ src/main/java/estudo/appengine/servlet/Exemplo1Servlet.java
@@ -0,0 +1,17 @@
+package estudo.appengine.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(name = "Exemplo1Servlet", value = "/exemplo1")
+public class Exemplo1Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
